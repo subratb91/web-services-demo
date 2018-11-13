05204c11c41ac9d4e3af1e26e814239cd307e01a
subrat91 <skb@outlook.in>
2018-11-13 08:25:02 +0530

    Implemented support for xml. Added documentation using Swagger

--- RestfulWebServices/src/main/java/com/subrat/demo/rest/webservices/RestfulWebServicesApplication.java
+++ RestfulWebServices/src/main/java/com/subrat/demo/rest/webservices/RestfulWebServicesApplication.java
@@ -1,15 +1,21 @@
 package com.subrat.demo.rest.webservices;
 
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import com.subrat.demo.rest.webservices.util.UserUtility;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication implements CommandLineRunner {
-	
+
 	@Autowired
 	private UserUtility userUtility;
 
@@ -20,5 +26,19 @@ public class RestfulWebServicesApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		userUtility.createUsers();
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 }
--- RestfulWebServices/src/main/java/com/subrat/demo/rest/webservices/entity/User.java
+++ RestfulWebServices/src/main/java/com/subrat/demo/rest/webservices/entity/User.java
@@ -10,15 +10,21 @@ import javax.persistence.Table;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 @Entity
 @Table(name = "tb_users")
+@ApiModel(description="All Details about the user.")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	@Size(min=2, message="Name shoud have at least 2 characters")
+	@ApiModelProperty(notes="Name shoud have at least 2 characters")
 	private String name;
 	@Past(message="Birth Date cannot be greater than or equal to current date")
+	@ApiModelProperty(notes="Birth dates should be in past")
 	private Date birthDate;
 
 	public User() {
